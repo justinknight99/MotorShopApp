e91be677bd526220200e9d067a078e67ac65c8ea
rafi <rafiihzadi@gmail.com>
2019-05-23 14:12:22 +0700

    final push

--- src/pages/belanja/belanja.ts
+++ src/pages/belanja/belanja.ts
@@ -10,49 +10,63 @@ import { BelanjaRinciPage } from '../belanja-rinci/belanja-rinci';
 })
 export class BelanjaPage {
 
-  dataBelanja = [
-  {
-    id: 0,
-    nama : "Standar Paddock Universal CBR 150 Ninja R",
-    foto : "https://ecs7.tokopedia.net/img/cache/700/product-1/2017/10/19/0/0_9eca619e-31a5-4fe1-a7d8-63ad27241aa3_520_390.jpg",
-    price : "Rp228.750",
-    favorit : false
-  },
-  {
-    id: 1,
-    nama : "Lampu LED Projie motor tornado double AE+DE (Blue+Red)",
-    foto : "https://s2.bukalapak.com/img/7834729601/w-1000/Lampu_LED_Projie_Tornado_Double_AE___DE__Red_Blue___Biru_.jpg",
-    price : "Rp71.683 Rp99.900-28%",
-    favorit : false
-  },
-  {
-    id: 2,
-    nama : "Shock DBS Merah Tabung Silver",
-    foto : "https://s4.bukalapak.com/img/9231452162/w-1000/SHOCK_DBS_ULIR_MERAH.jpg",
-    price : "Rp175.084 Rp250.000-30%",
-    favorit : false
-  },
-  {
-    id: 3,
-    nama : "spion standar model mio",
-    foto : "https://s1.bukalapak.com/img/1263417242/w-1000/Spion_Standar_Yamaha_Model_Jupiter___Spion_.png",
-    price : "Rp20.500",
-    favorit : false
-  },
-  {
-    id: 4,
-    nama : "Saklar Wrna Tombol Khusus Motor Honda Yang Ada Tombol Idlingnya Saklar Tombol On/off Warna Biru",
-    foto : "https://id-test-11.slatic.net/original/eee623cb609ad25b23c6ff1f9163ae21.jpg_200x200q75.jpg_.webp",
-    price : "Rp55.000 Rp90.000-39%",
-    favorit : false
-  },
-  {
-    id: 5,
-    nama : "Hanfat Handgrip Daytona black grey 2738 universal mio beat vario jupiter cbr vixion nmax",
-    foto : "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/2/26/49146268/49146268_e490cf48-9790-46d0-8936-1fbc9b5952ea_640_640.jpg",
-    price : "Rp69.900",
-    favorit : false
-  },
+  barang = [
+    {
+      id : 0,
+      nama : "Harga KTC Proguard UniversalJalu Setang Motor - Red",
+      foto : "https://s4.bukalapak.com/img/9231452162/w-300/SHOCK_DBS_ULIR_MERAH.jpg.webp",
+      price : "Rp37.905",
+      favorit : false,
+    },
+    {
+      id : 1,
+      nama : "LED Projie double AE dan DE",
+      foto : "https://s3.bukalapak.com/img/3174548196/w-300/LAMPU_LED_PROJECTOR_U8_DOUBLE_AE___DE___PROJIE_LED_DAN_AE_CO.jpg.webp",
+      price : "Rp61.750",
+      favorit : false,
+    },
+    {
+      id : 2,
+      nama : "Paketan Velg Cross - Paketan Velg Trail Ring 14 -17 Sem",
+      foto : "https://s2.bukalapak.com/img/2446284971/w-300/Paketan_Velg_Cross___Paketan_Velg_Trail_Ring_14__17_Matic_Xr.jpg.webp",
+      price : "Rp1.160.999",
+      favorit : false,
+    },
+    {
+      id : 3,
+      nama : "Saklar Wrna Tombol Khusus Motor Honda Universal Saklar Tombol On/off Warna Biru",
+      foto : "https://id-test-11.slatic.net/original/eee623cb609ad25b23c6ff1f9163ae21.jpg_200x200q75.jpg_.webp",
+      price : "Rp50.550",
+      favorit : false,
+    },
+    {
+      id : 4,
+      nama : "Harga KTC Proguard UniversalJalu Setang Motor - Red",
+      foto : "https://s4.bukalapak.com/img/9231452162/w-300/SHOCK_DBS_ULIR_MERAH.jpg.webp",
+      price : "Rp37.905",
+      favorit : false,
+    },
+    {
+      id : 5,
+      nama : "LED Projie double AE dan DE",
+      foto : "https://s3.bukalapak.com/img/3174548196/w-300/LAMPU_LED_PROJECTOR_U8_DOUBLE_AE___DE___PROJIE_LED_DAN_AE_CO.jpg.webp",
+      price : "Rp61.750",
+      favorit : false,
+    },
+    {
+      id : 6,
+      nama : "Paketan Velg Cross - Paketan Velg Trail Ring 14 -17 Sem",
+      foto : "https://s2.bukalapak.com/img/2446284971/w-300/Paketan_Velg_Cross___Paketan_Velg_Trail_Ring_14__17_Matic_Xr.jpg.webp",
+      price : "Rp1.160.999",
+      favorit : false,
+    },
+    {
+      id : 7,
+      nama : "Saklar Wrna Tombol Khusus Motor Honda Universal Saklar Tombol On/off Warna Biru",
+      foto : "https://id-test-11.slatic.net/original/eee623cb609ad25b23c6ff1f9163ae21.jpg_200x200q75.jpg_.webp",
+      price : "Rp50.550",
+      favorit : false,
+    }
   ]
 
   
@@ -68,7 +82,7 @@ export class BelanjaPage {
   }
 
   ubahFav(data){
-    this.dataBelanja[data].favorit = !this.dataBelanja[data.id].favorit
+    this.barang[data.id].favorit = !this.barang[data.id].favorit
   }
   bukaRinci(data){
     this.app.getRootNav().push(BelanjaRinciPage, data)
--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -8,6 +8,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HistoryPage {
 
+  bengkel = [
+    {
+      nama : "Bengkel Motor 3 Putra" ,
+      alamat : "Jl. R. H. Moh. Tohir, Tegal Gundil, Kec. Bogor Utara, Kota Bogor, Jawa Barat 16152",
+      tanggal : "23 Mei 2019"
+    },
+    {
+      nama : "Diana motor - Jual beli mobil bekas" ,
+      alamat : "Jl. Sindang Barang Pengkolan No.50a, Sindangbarang, Bogor Bar., Kota Bogor, Jawa Barat 16117",
+      tanggal : "23 Mei 2019"
+    },
+    {
+      nama : "Bengkel Motor 7RD" ,
+      alamat : "Villa Ciomas Indah Blok N-7 Nomor 12, Jalan Garuda Raya, Ciomas Rahayu, Ciomas, Bogor, Jawa Barat 16610",
+      tanggal : "23 Mei 2019"
+    },
+    {
+      nama : "Kawasaki JAR" ,
+      alamat : "Jl. Batutulis No.91, Batutulis, Kec. Bogor Sel., Kota Bogor, Jawa Barat 16133",
+      tanggal : "23 Mei 2019"
+    },
+    {
+      nama : "PT. Murni Motor - Pajajaran" ,
+      alamat : "Jl. Pajajaran Indah No.78Q, Baranangsiang, Bogor Tim., Kota Bogor, Jawa Barat 16143",
+      tanggal : "23 Mei 2019"
+    },
+    {
+      nama : "Kabayan Klasik Custom" ,
+      alamat : "Jl. Kapten Yusuf No.15, Cikaret, Kec. Bogor Sel., Kota Bogor, Jawa Barat 16132",
+      tanggal : "23 Mei 2019"
+    },
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
